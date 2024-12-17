1d32c46e61347965d9c153b2b68ae6498af608c7
Carlos Estrada <carlosestrada94@outlook.com>
2024-12-16 22:38:28 -0500

    finish sliding window lecture and add jest for excercise tests

--- arrays_and_strings/2_sliding_window/index.ts
+++ arrays_and_strings/2_sliding_window/index.ts
@@ -71,28 +71,6 @@ function implementationExample(nums: number[], k: number): number {
  * What is the length of the longest substring achievable that contains only "1"?
  */
 
-/* function longestSubstring(s: string): number {
-  let left = 0;
-  let ans = 0;
-  let zeroIndex: number | null = null;
-
-  for (let right = 0; right < s.length; right++) {
-    if (s[right] === "1" || (s[right] === "0" && zeroIndex === null)) {
-      ans = Math.max(ans, right - left + 1);
-    }
-
-    if (zeroIndex !== null && s[right] === "0") {
-      left = zeroIndex + 1;
-    }
-
-    if (s[right] === "0") {
-      zeroIndex = right;
-    }
-  }
-
-  return ans;
-} */
-
 function longestSubstring(s: string): number {
   let left = 0;
   let count = 0;
@@ -116,9 +94,96 @@ function longestSubstring(s: string): number {
   return ans;
 }
 
-console.log(longestSubstring("1101100111")); // 5
-console.log(longestSubstring("11111")); // 5
-console.log(longestSubstring("1110111")); // 7
-console.log(longestSubstring("0")); // 1
-console.log(longestSubstring("000")); // 1
-console.log(longestSubstring("01010101")); // 3
+// console.log(longestSubstring("1101100111")); // 5
+// console.log(longestSubstring("11111")); // 5
+// console.log(longestSubstring("1110111")); // 7
+// console.log(longestSubstring("0")); // 1
+// console.log(longestSubstring("000")); // 1
+// console.log(longestSubstring("01010101")); // 3
+
+/**
+ * Number of subarrays
+ *
+ * We can use the Sliding window method when the problem asks for the number of subarrays that fit some constraint.
+ * However, we need to use a trick to calculate the number of subarrays:
+ *    - We have a window defined by (left, right). How many valid windows end at index right?
+ *        - The current window is valid (left, right)
+ *        - Then (left + 1, right), (left + 2, right) ... (right, right)
+ *        - The statement above means that when right is fixed, left could take any value from left to right inclusive.
+ *          Therefore, the number of valid windows ending at index right is equal to the size of the window right - left + 1.
+ */
+
+/**
+ * Example:
+ *
+ * Given an array of positive integers nums and an integer k, return the number of subarrays where the product of all the elements
+ * in the subarray is strictly less than k.
+ *
+ * For example, given the input nums = [10, 5, 2, 6], k = 100, the answer is 8: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+ */
+
+function numberOfSubarraysWithProductLessThanK(
+  nums: number[],
+  k: number
+): number {
+  if (k <= 1) {
+    return 0; // Because all numbers in nums, are >= 1. Therefore, there is no window with product < 1
+  }
+
+  let left = 0;
+  let product = 1;
+  let ans = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    product *= nums[right];
+
+    while (product >= k) {
+      product /= nums[left];
+      left++;
+    }
+
+    ans += right - left + 1;
+  }
+
+  return ans;
+}
+
+// console.log(numberOfSubarraysWithProductLessThanK([10, 5, 2, 6], 100));
+
+/**
+ * Fixed window size
+ *
+ * Sometimes, the problem will specify a fixed length k for the window.
+ * These problems are easier, because the difference between adjacent window is only two elements:
+ *    - We add one element to the right.
+ *    - Remove one element to the left.
+ *
+ * The algorithm:
+ *    1. Build the first window of length k, from index 0 to index k-1
+ *    2. Then, add element at index i, and remove element at index i - k
+ */
+
+/**
+ * Example:
+ * Given an integer array nums and an integer k, find the sum of the subarray with the largest sum whose length is k.
+ */
+
+function findLargestSum(nums: number[], k: number) {
+  let sum = 0;
+  let ans = 0;
+
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+
+  ans = sum;
+
+  for (let i = k; i < nums.length; i++) {
+    sum += nums[i] - nums[i - k];
+    ans = Math.max(ans, sum);
+  }
+
+  return ans;
+}
+
+console.log(findLargestSum([3, -1, 4, 12, -8, 5, 6], 4));
